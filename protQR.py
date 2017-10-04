1d500175af7b824238be0e2a669189505d74548a
Jeff Cole <coleti16@students.ecu.edu>
2017-10-04 15:27:19 -0400

    new

--- protQR.py
+++ protQR.py
@@ -1,104 +1,100 @@
 # import PIL.ImageDraw as ImageDraw,PIL.Image as Image, PIL.ImageShow as ImageShow
 import random
+import numpy as np
+
+import sys
+from itertools import groupby
+
+def getOptionValue(option):
+    optionPos = [i for i, j in enumerate(sys.argv) if j == option][0]
+    optionValue = sys.argv[optionPos + 1]
+    return optionValue
+
+if "-i" in sys.argv:
+    inFile = getOptionValue("-i")
+else:
+    # input_directory = "data/"
+    print("\nplease specify input file name using -i <file_name> \n")
+    sys.exit()
+
+if "-o" in sys.argv:
+    outFile = getOptionValue("-o")
+else:
+    # input_directory = "data/"
+    print("\nplease specify output file name using -o <file_name> \n")
+    sys.exit()
+
+
 sequence = "CRREEQLRIQRSELLEGRNPRGVKMRWLLGIFLIALLACASAKKIPQAEDTEKDQLDLIEEDDNLNEVESNDDVASNEVAGGYTPDPCLKVRCGAGRVCEVNDKGEGECVCIPECPQETDDRRKVCSNHNETWNSDCEVYQMRCYCAEDTEECKTKTYKHVHVDYYGECRDIPKCSEEEMEDFPRRMREWLFNIMQDLAQRSELDDPYLELEKEAERDLAKKWSNAVIWKFCDLDSHPFDRSVSRHELFPIRAPLLAMEHCIAPFLDKCDADDDHRISLKEWGLCLGLKENEIEDKCSAIRDNQ"
 sequence1 = "KGDLACYFFHLIPFVCEICHCKSSKMAPPCFTELGKDAKDIFSKGYNFSLVKLDCKTKTRGGMEFTVSGSSNTESGKVSSSLETKYKVPEYGMTLKEKWTTDNVLSTEVAVEDKLIKGSKFSFNGTFVPLTGKKSGVLKSAFKAENVHLNADVDLDMKGPLIHCASVLGLKGWLFGAQSSFDTCKSKVSRCNFALGYSTDDFVLHTNVNDGQEFGAAIYQKVDSNLETGVQLGWAAGNNATAFGLGCVYSLDKDTSLRAKINNTSQIGLGITHKLRDGIKLTLSAMIDGRSFNQGGHKLGIGLDLEA"
-rando = ""
 sequence2 = "AAACCCCA"
-aminoAcids = "ACDEFGHIKLMNPQRSTVWY"
-for i in range(300):
-    rando += random.choice(aminoAcids)
-#print(rando)
-number = 0
-aa2merPos = {}
-for row in aminoAcids:
-    for column in aminoAcids:
-        # print(row,column, number)
-        aa2merPos[row+column]=number
-        number+=1
 
-# print(aa2merPos)
 
 def makeKmers(st,k):
     kmerList = []
     for i in range(len(st)-k+1):
         kmer = st[i:i+k]
         kmerList.append(kmer)
-        # if kmer not in kmerDict:
-        #     kmerDict[kmer] = 1
-        # else:
-        #     kmerDict[kmer]+=1
     return kmerList
-# print(makeKmers(sequence2,3))
-pairs = {}
+
 def kBreuin(st,n):
-    print("BEGIN")
-    dt = makeKmers(st,n)
-    for i in dt:
-        # print(i,n)
-        print(makeKmers(i,n-1))
-
-
-        if(n-1>=3):
-            kBreuin(i,n-1)
+    kGraph = {}
+    nmers = makeKmers(st,n)
+    for i in nmers:
+        minus1mers = makeKmers(i,n-1)
+        merPair = (minus1mers[0],minus1mers[1])
+        if merPair not in kGraph:
+            kGraph[merPair] = 1
         else:
-            print(i,"FAIL",makeKmers(i,n-1))
-            dupley = (makeKmers(i,n-1)[0],makeKmers(i,n-1)[1])
-            print(dupley)
-            print(dupley in pairs)
-            try:
-                pairs[dupley]+=1
-            except:
-                pairs[dupley]=1
-        print(pairs)
-            # try:
-            #     pairs((makeKmers(i,n-1)[0],makeKmers(i,n-1)[1]))+=1
-            # except:
-            #     pairs((makeKmers(i,n-1)[0],makeKmers(i,n-1)[1]))=1
+            kGraph[merPair] +=1
+    return kGraph
 
-        # try:
-        #     print(kBreuin(i,n-1))
-        # except:
-        #     print(makeKmers(i,n-1))
-            # try:
-            #     # pairs[makeKmers(i,n-1)] = 1
-            # except:
+def printMe(st):
+    aminoAcids = "ACDEFGHIKLMNPQRSTVWY"
+    number = 0
+    aa2merPos = {}
+    line2print = ""
 
-                # pairs[makeKmers(i,n-1)] += 1
-    # return pairs
+    #NOTE this can also be generalized if number of loops is indicated by k in kmer, makes huge list though as k goes up
+    for row in aminoAcids:
+        for column in aminoAcids:
+            aa2merPos[row+column]=number
+            number+=1
+    kB = kBreuin(st,2)
+    kList =  np.zeros((len(aa2merPos)))
+    # print(len(aa2merPos))
+    for i in kB:
+        pair = i[0]+i[1]
+        kList[aa2merPos[pair]]=kB[i]
+    for i in kList:
+        line2print+=str(i)+","
+    # print(line2print)
+    return line2print[:-1]
+# print(printMe(sequence))
+titleList = np.zeros((400))
+aa2merPos = {}
+line2print = ""
 
-# def deBleh(st,k):
-#     dt = makeKmers(st,k)
-#     deBruignGraph = {}
-#     for i in dt:
-#         # nextDownMer = makeKmers(i,k-1)
-#         nextMerPair = (i[0:2],i[1:3])
-#         if nextMerPair not in deBruignGraph:
-#             deBruignGraph[nextMerPair] = 1
-#         else:
-#             deBruignGraph[nextMerPair] += 1
-#     return deBruignGraph
-print(sequence2)
-print(makeKmers(sequence2,3))
-print(kBreuin(sequence2,6))
-# print(deBleh(sequence2,3))
-# print(deBleh(sequence2,3))
-# seqDeBleh = deBleh(rando,3)
-# im = Image.new("RGB", (400,400))
-# draw = ImageDraw.Draw(im)
-# draw.rectangle ((0, 400, 400, 0), (95,215,214))
-# for i in seqDeBleh.keys():
-#     print(aa2merPos[i[0]],aa2merPos[i[1]],seqDeBleh[i])
-#
-#
-#
-#
-#     draw.point((aa2merPos[i[0]],aa2merPos[i[1]]))
+#NOTE this can also be generalized if number of loops is indicated by k in kmer, makes huge list though as k goes up
+for row in aminoAcids:
+    for column in aminoAcids:
+        aa2merPos[row+column]=number
+        number+=1
+for i in aa2merPos:
+    titleList[aa2merPos[i]] = i
+title = ""
+for i in titleList:
+    title = i+","
+for i in
+with open(outFile,"w") as out:
+    out.write(title[:-1]+"\n")
+    sequence_iterator = fasta_iter(inFile)
+    for ff in sequence_iterator:
+        headerStr, seq = ff
+        out.write(headerStr+","+printMe(seq)+"\n")
+# print(len(printMe(sequence).split(",")))
+    # kList[aa2merPos[pair]]=kB[i]
 
-#Background & Island
+# print(kList)
 
-# draw.ellipse ((50, 170, 350, 250), (29,108,29))
-# draw.ellipse ((300, 30, 360, 90), (245,245,89))
-# draw.rectangle ((0, 300, 400, 200), (15,16,134))
-
-
-# im.show()
+# kList[1]=1
