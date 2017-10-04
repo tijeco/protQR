2253fe955b7dab703df122d699c2ee4ffa56cb61
Jeff Cole <coleti16@students.ecu.edu>
2017-10-04 12:09:51 -0400

    new

--- protQR.py
+++ protQR.py
@@ -1,4 +1,4 @@
-import PIL.ImageDraw as ImageDraw,PIL.Image as Image, PIL.ImageShow as ImageShow
+# import PIL.ImageDraw as ImageDraw,PIL.Image as Image, PIL.ImageShow as ImageShow
 import random
 sequence = "CRREEQLRIQRSELLEGRNPRGVKMRWLLGIFLIALLACASAKKIPQAEDTEKDQLDLIEEDDNLNEVESNDDVASNEVAGGYTPDPCLKVRCGAGRVCEVNDKGEGECVCIPECPQETDDRRKVCSNHNETWNSDCEVYQMRCYCAEDTEECKTKTYKHVHVDYYGECRDIPKCSEEEMEDFPRRMREWLFNIMQDLAQRSELDDPYLELEKEAERDLAKKWSNAVIWKFCDLDSHPFDRSVSRHELFPIRAPLLAMEHCIAPFLDKCDADDDHRISLKEWGLCLGLKENEIEDKCSAIRDNQ"
 sequence1 = "KGDLACYFFHLIPFVCEICHCKSSKMAPPCFTELGKDAKDIFSKGYNFSLVKLDCKTKTRGGMEFTVSGSSNTESGKVSSSLETKYKVPEYGMTLKEKWTTDNVLSTEVAVEDKLIKGSKFSFNGTFVPLTGKKSGVLKSAFKAENVHLNADVDLDMKGPLIHCASVLGLKGWLFGAQSSFDTCKSKVSRCNFALGYSTDDFVLHTNVNDGQEFGAAIYQKVDSNLETGVQLGWAAGNNATAFGLGCVYSLDKDTSLRAKINNTSQIGLGITHKLRDGIKLTLSAMIDGRSFNQGGHKLGIGLDLEA"
@@ -29,30 +29,70 @@ def makeKmers(st,k):
         #     kmerDict[kmer]+=1
     return kmerList
 # print(makeKmers(sequence2,3))
-
-def deBleh(st,k):
-    dt = makeKmers(st,k)
-    deBruignGraph = {}
+pairs = {}
+def kBreuin(st,n):
+    print("BEGIN")
+    dt = makeKmers(st,n)
     for i in dt:
-        # nextDownMer = makeKmers(i,k-1)
-        nextMerPair = (i[0:2],i[1:3])
-        if nextMerPair not in deBruignGraph:
-            deBruignGraph[nextMerPair] = 1
+        # print(i,n)
+        print(makeKmers(i,n-1))
+
+
+        if(n-1>=3):
+            kBreuin(i,n-1)
         else:
-            deBruignGraph[nextMerPair] += 1
-    return deBruignGraph
+            print(i,"FAIL",makeKmers(i,n-1))
+            dupley = (makeKmers(i,n-1)[0],makeKmers(i,n-1)[1])
+            print(dupley)
+            print(dupley in pairs)
+            try:
+                pairs[dupley]+=1
+            except:
+                pairs[dupley]=1
+        print(pairs)
+            # try:
+            #     pairs((makeKmers(i,n-1)[0],makeKmers(i,n-1)[1]))+=1
+            # except:
+            #     pairs((makeKmers(i,n-1)[0],makeKmers(i,n-1)[1]))=1
+
+        # try:
+        #     print(kBreuin(i,n-1))
+        # except:
+        #     print(makeKmers(i,n-1))
+            # try:
+            #     # pairs[makeKmers(i,n-1)] = 1
+            # except:
+
+                # pairs[makeKmers(i,n-1)] += 1
+    # return pairs
+
+# def deBleh(st,k):
+#     dt = makeKmers(st,k)
+#     deBruignGraph = {}
+#     for i in dt:
+#         # nextDownMer = makeKmers(i,k-1)
+#         nextMerPair = (i[0:2],i[1:3])
+#         if nextMerPair not in deBruignGraph:
+#             deBruignGraph[nextMerPair] = 1
+#         else:
+#             deBruignGraph[nextMerPair] += 1
+#     return deBruignGraph
+print(sequence2)
+print(makeKmers(sequence2,3))
+print(kBreuin(sequence2,6))
 # print(deBleh(sequence2,3))
-seqDeBleh = deBleh(rando,3)
-im = Image.new("RGB", (400,400))
-draw = ImageDraw.Draw(im)
-draw.rectangle ((0, 400, 400, 0), (95,215,214))
-for i in seqDeBleh.keys():
-    print(aa2merPos[i[0]],aa2merPos[i[1]],seqDeBleh[i])
-
-
-
-
-    draw.point((aa2merPos[i[0]],aa2merPos[i[1]]))
+# print(deBleh(sequence2,3))
+# seqDeBleh = deBleh(rando,3)
+# im = Image.new("RGB", (400,400))
+# draw = ImageDraw.Draw(im)
+# draw.rectangle ((0, 400, 400, 0), (95,215,214))
+# for i in seqDeBleh.keys():
+#     print(aa2merPos[i[0]],aa2merPos[i[1]],seqDeBleh[i])
+#
+#
+#
+#
+#     draw.point((aa2merPos[i[0]],aa2merPos[i[1]]))
 
 #Background & Island
 
