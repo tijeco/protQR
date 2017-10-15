1319605ddcf7e56930be4ec628309be57e4a3401
Jeff Cole <coleti16@students.ecu.edu>
2017-10-15 17:29:53 -0400

    new

--- centroid.py
+++ centroid.py
@@ -79,7 +79,7 @@ with open(inFile.strip().split("/")[-1].split(".")[0]+"_"+prefix,"w") as out:
     sequence_iterator = fasta_iter(inFile)
     for ff in sequence_iterator:
         headerStr, seq = ff
-        print(seq)
+        # print(seq)
         line2write = ""
         if prefix == "noLen.csv":
             try:
