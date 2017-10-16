6af029c9bf49758607e75740c5da8aa9aa65888c
Jeff Cole <coleti16@students.ecu.edu>
2017-10-16 08:52:23 -0400

    raw

--- tf.iris.py
+++ tf.iris.py
@@ -24,14 +24,14 @@ IRIS_TEST_URL = "http://download.tensorflow.org/data/iris_test.csv"
 
 # If the training and test sets aren't stored locally, download them.
 if not os.path.exists(IRIS_TRAINING):
-  raw = urllib.request.urlopen(IRIS_TRAINING_URL).read()
-  with open(IRIS_TRAINING, "w") as f:
-    f.write(raw)
+  with urllib.request.urlopen(IRIS_TRAINING_URL) as raw:
+      with open(IRIS_TRAINING, "w") as f:
+          f.write(raw)
 
 if not os.path.exists(IRIS_TEST):
-  raw = urllib.request.urlopen(IRIS_TEST_URL).read()
-  with open(IRIS_TEST, "w") as f:
-    f.write(raw)
+  with urllib.request.urlopen(IRIS_TEST_URL) as raw:
+      with open(IRIS_TEST, "w") as f:
+          f.write(raw)
   # Load datasets.
 
 training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
