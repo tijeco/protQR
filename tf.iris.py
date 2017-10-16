94c36e0230231d944f9e75ebe3ce705fb3d7011f
Jeff Cole <coleti16@students.ecu.edu>
2017-10-16 07:48:46 -0400

    get data

--- tf.iris.py
+++ tf.iris.py
@@ -15,3 +15,16 @@ import urllib
 
 import numpy as np
 import tensorflow as tf
+
+
+def main():
+  # If the training and test sets aren't stored locally, download them.
+  if not os.path.exists(IRIS_TRAINING):
+    raw = urllib.urlopen(IRIS_TRAINING_URL).read()
+    with open(IRIS_TRAINING, "w") as f:
+      f.write(raw)
+
+  if not os.path.exists(IRIS_TEST):
+    raw = urllib.urlopen(IRIS_TEST_URL).read()
+    with open(IRIS_TEST, "w") as f:
+      f.write(raw)
