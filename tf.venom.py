f621b97ac48d32759ffa903ce4ef90abd4050728
Jeff Cole <coleti16@students.ecu.edu>
2017-10-16 09:35:21 -0400

    to the walls

---
+++ tf.venom.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+# conda execute
+# env:
+#  - python >=3
+#  - numpy
+#  - tensorflow
+
+
+
+import os
+import urllib.request
+
+import numpy as np
+import tensorflow as tf
+training_data = "venom.train.csv"
+test_data = "venom.test.csv"
+
+
+training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+    filename=training_data,
+    target_dtype=np.int,
+    features_dtype=np.float32)
+test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+    filename=test_data,
+    target_dtype=np.int,
+    features_dtype=np.float32)
+feature_columns = [tf.feature_column.numeric_column("x", shape=[6])]
+classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
+                                  hidden_units=[10, 20, 10],
+                                      n_classes=4,
+                                      model_dir="tmp/venom_model")
+train_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={"x": np.array(training_set.data)},
+    y=np.array(training_set.target),
+    num_epochs=None,
+    shuffle=True)
+classifier.train(input_fn=train_input_fn, steps=2000)
+
+test_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={"x": np.array(test_set.data)},
+    y=np.array(test_set.target),
+    num_epochs=1,
+    shuffle=False)
+accuracy_score = classifier.evaluate(input_fn=test_input_fn)["accuracy"]
+
+print("\nTest Accuracy: {0:f}\n".format(accuracy_score))
+
+
+"""
+49044 venom.train.csv
+21020 venom.test.csv
+120,4,setosa,versicolor,virginica
+
+"""
