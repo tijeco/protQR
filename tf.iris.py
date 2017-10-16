350568a22c006041cc7e55f260f7cd8a465401fa
Jeff Cole <coleti16@students.ecu.edu>
2017-10-16 07:45:17 -0400

    tensorflow iris

---
+++ tf.iris.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import urllib
+
+import numpy as np
+import tensorflow as tf
+
+# conda execute
+# env:
+#  - python >=3
+#  - numpy
+#  - tensorflow
