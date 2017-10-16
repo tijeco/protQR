449f1983393659785ae882f12363bc45d895eef1
Jeff Cole <coleti16@students.ecu.edu>
2017-10-16 08:02:25 -0400

    work

--- tf.iris.py
+++ tf.iris.py
@@ -6,9 +6,7 @@
 #  - numpy
 #  - tensorflow
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+
 
 import os
 import urllib
@@ -23,37 +21,35 @@ IRIS_TRAINING_URL = "http://download.tensorflow.org/data/iris_training.csv"
 IRIS_TEST = "iris_test.csv"
 IRIS_TEST_URL = "http://download.tensorflow.org/data/iris_test.csv"
 
-def main():
-    # If the training and test sets aren't stored locally, download them.
-    if not os.path.exists(IRIS_TRAINING):
-      raw = urllib.urlopen(IRIS_TRAINING_URL).read()
-      with open(IRIS_TRAINING, "w") as f:
-        f.write(raw)
 
-    if not os.path.exists(IRIS_TEST):
-      raw = urllib.urlopen(IRIS_TEST_URL).read()
-      with open(IRIS_TEST, "w") as f:
-        f.write(raw)
-      # Load datasets.
+# If the training and test sets aren't stored locally, download them.
+if not os.path.exists(IRIS_TRAINING):
+  raw = urllib.urlopen(IRIS_TRAINING_URL).read()
+  with open(IRIS_TRAINING, "w") as f:
+    f.write(raw)
 
-    training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
-        filename=IRIS_TRAINING,
-        target_dtype=np.int,
-        features_dtype=np.float32)
-    test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
-        filename=IRIS_TEST,
-        target_dtype=np.int,
-        features_dtype=np.float32)
-    # Specify that all features have real-value data
+if not os.path.exists(IRIS_TEST):
+  raw = urllib.urlopen(IRIS_TEST_URL).read()
+  with open(IRIS_TEST, "w") as f:
+    f.write(raw)
+  # Load datasets.
+
+training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+    filename=IRIS_TRAINING,
+    target_dtype=np.int,
+    features_dtype=np.float32)
+test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+    filename=IRIS_TEST,
+    target_dtype=np.int,
+    features_dtype=np.float32)
 # Specify that all features have real-value data
 # Specify that all features have real-value data
-    feature_columns = [tf.feature_column.numeric_column("x", shape=[4])]
-    print(feature_columns)
+# Specify that all features have real-value data
+feature_columns = [tf.feature_column.numeric_column("x", shape=[4])]
+print(feature_columns)
 
-  # Build 3 layer DNN with 10, 20, 10 units respectively.
-    classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
-                                          hidden_units=[10, 20, 10],
-                                          n_classes=3,
-                                          model_dir="/tmp/iris_model")
-if __name__ == "__main__":
-    main()
+# Build 3 layer DNN with 10, 20, 10 units respectively.
+classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
+                                      hidden_units=[10, 20, 10],
+                                      n_classes=3,
+                                      model_dir="/tmp/iris_model")
