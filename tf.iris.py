5f2d2d13ea6c3055fc55bee0d3111a6cbf247ab8
Jeff Cole <coleti16@students.ecu.edu>
2017-10-16 07:56:31 -0400

    nope

--- tf.iris.py
+++ tf.iris.py
@@ -24,22 +24,23 @@ IRIS_TEST = "iris_test.csv"
 IRIS_TEST_URL = "http://download.tensorflow.org/data/iris_test.csv"
 
 def main():
-  # If the training and test sets aren't stored locally, download them.
-  if not os.path.exists(IRIS_TRAINING):
-    raw = urllib.urlopen(IRIS_TRAINING_URL).read()
-    with open(IRIS_TRAINING, "w") as f:
-      f.write(raw)
+    # If the training and test sets aren't stored locally, download them.
+    if not os.path.exists(IRIS_TRAINING):
+      raw = urllib.urlopen(IRIS_TRAINING_URL).read()
+      with open(IRIS_TRAINING, "w") as f:
+        f.write(raw)
 
-  if not os.path.exists(IRIS_TEST):
-    raw = urllib.urlopen(IRIS_TEST_URL).read()
-    with open(IRIS_TEST, "w") as f:
-      f.write(raw)
-    # Load datasets.
-  training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
-      filename=IRIS_TRAINING,
-      target_dtype=np.int,
-      features_dtype=np.float32)
-  test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
-      filename=IRIS_TEST,
-      target_dtype=np.int,
-      features_dtype=np.float32)
+    if not os.path.exists(IRIS_TEST):
+      raw = urllib.urlopen(IRIS_TEST_URL).read()
+      with open(IRIS_TEST, "w") as f:
+        f.write(raw)
+      # Load datasets.
+
+    training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+        filename=IRIS_TRAINING,
+        target_dtype=np.int,
+        features_dtype=np.float32)
+    test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+        filename=IRIS_TEST,
+        target_dtype=np.int,
+        features_dtype=np.float32)
