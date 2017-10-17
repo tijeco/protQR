2da5f2303623218e95e4648bd290ede4c311abfc
Jeff Cole <coleti16@students.ecu.edu>
2017-10-17 15:45:16 -0400

    maybe

--- tf.venom.py
+++ tf.venom.py
@@ -27,12 +27,11 @@ test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
     features_dtype=np.float32)
 feature_columns = [tf.feature_column.numeric_column("x", shape=[6])]
 print(feature_columns)
-print(np.isnan(feature_columns).any(),"THINGS@@@@@@@@@@@@@@@@@")
 classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
                                   hidden_units=[500,500,500,500],
                                       n_classes=2,
                                       model_dir="tmp/venom_model",
-                                      optimizer="SGD"
+                                      optimizer=tf.train.ProximalAdagradOptimizer(learning_rate=0.001, l1_regularization_strength=0.00001)
                                       )
                                       #Adagrad', 'Adam', 'Ftrl', 'RMSProp', 'SGD'
 
