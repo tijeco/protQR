194ed5b5561a8c9c2a0ac7c18cfa886be165e59d
Jeff Cole <coleti16@students.ecu.edu>
2017-10-17 15:18:58 -0400

    Ftrl

--- tf.venom.py
+++ tf.venom.py
@@ -31,7 +31,7 @@ classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
                                   hidden_units=[500,500,500,500],
                                       n_classes=2,
                                       model_dir="tmp/venom_model",
-                                      optimizer="Adagrad"
+                                      optimizer="Ftrl"
                                       )
                                       #Adagrad', 'Adam', 'Ftrl', 'RMSProp', 'SGD'
 
