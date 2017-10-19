da6b9f0b4c3753f36a4a8e62e4aae70ed192d0d0
Jeff Cole <coleti16@students.ecu.edu>
2017-10-19 07:26:53 -0400

    more layers less neurons

--- tf.venom.py
+++ tf.venom.py
@@ -31,7 +31,7 @@ test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
 feature_columns = [tf.feature_column.numeric_column("x", shape=[6])]
 print(feature_columns)
 classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
-                                  hidden_units=[500,500,500,500],
+                                  hidden_units=[50,50,50,50,50,50,50],
                                       n_classes=2,
                                       model_dir="tmp/venom_model",
                                     #   optimizer=tf.train.ProximalAdagradOptimizer(learning_rate=0.01, l1_regularization_strength=0.001)
