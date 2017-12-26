9506008741c9805d5f8903ff53bde3343e098341
Jeff Cole <coleti16@students.ecu.edu>
2017-12-26 18:46:43 -0500

    normalize dict

---
+++ normalize.py
@@ -0,0 +1,24 @@
+import sys
+line1 = True
+max_dict = {}
+min_dict = {}
+with open(sys.argv[1]) as f:
+    if line1:
+        line1 == False
+        continue
+    for line in f:
+        row = line.strip().split(",")
+        for i in range(len(row[-1])):
+            if i in min_dict:
+                if min_dict[i] > row[i]:
+                    min_dict[i] = row[i]
+            else:
+                min_dict = row[i]
+            if i in max_dict:
+                if max_dict[i] < row[i]:
+                    max_dict[i] = row[i]
+                else:
+                    max_dict = row[i]
+
+print(min_dict)
+print(max_dict)            
