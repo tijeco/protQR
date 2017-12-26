251d69b58d6e6daa02102d98c49efa9997d5477c
Jeff Cole <coleti16@students.ecu.edu>
2017-12-26 18:58:14 -0500

    silly

--- normalize.py
+++ normalize.py
@@ -10,8 +10,8 @@ with open(sys.argv[1]) as f:
             if i in max_dict:
                 if max_dict[i] < float(row[i]):
                     max_dict[i] = float(row[i])
-                else:
-                    max_dict[i] = float(row[i])
+            else:
+                max_dict[i] = float(row[i])
             if i in min_dict:
                 if min_dict[i] > float(row[i]):
                     min_dict[i] = float(row[i])
