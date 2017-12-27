b52f48df93a9d611514017dda9d3b160c45cb93d
Jeff Cole <coleti16@students.ecu.edu>
2017-12-26 19:04:36 -0500

    now

--- normalize.py
+++ normalize.py
@@ -7,21 +7,20 @@ with open(sys.argv[1]) as f:
     for line in f:
         if line1:
             line1=False
-            print(line1)
-        else:
+            continue
 
-            row = line.strip().split(",")
-            for i in range(len(row[:-1])):
-                if i in max_dict:
-                    if max_dict[i] < float(row[i]):
-                        max_dict[i] = float(row[i])
-                else:
+        row = line.strip().split(",")
+        for i in range(len(row[:-1])):
+            if i in max_dict:
+                if max_dict[i] < float(row[i]):
                     max_dict[i] = float(row[i])
-                if i in min_dict:
-                    if min_dict[i] > float(row[i]):
-                        min_dict[i] = float(row[i])
-                else:
+            else:
+                max_dict[i] = float(row[i])
+            if i in min_dict:
+                if min_dict[i] > float(row[i]):
                     min_dict[i] = float(row[i])
+            else:
+                min_dict[i] = float(row[i])
 
 
 print(min_dict)
