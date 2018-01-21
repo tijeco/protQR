6f27f38547e889f7f272e9940a3a9ef8ff8e6e37
Jeff Cole <coleti16@students.ecu.edu>
2018-01-20 19:42:52 -0500

    paren

--- normalize.py
+++ normalize.py
@@ -40,6 +40,6 @@ with open(sys.argv[1].replace("csv","norm.csv"),"w") as out:
             for i in range(len(row[:-1])):
                 # 2* (x-min)/(max)
                 line2write+=str( 2.0*(float(row[i]) - min_dict[i]) / (max_dict[i]-min_dict[i]) -1.0)+","
-                print(str( 2.0*(float(row[i]) - min_dict[i]) / (max_dict[i]-min_dict[i]),min_dict[i],max_dict[i],row[i])
+                print(str( 2.0*(float(row[i]) - min_dict[i]) / (max_dict[i]-min_dict[i])),min_dict[i],max_dict[i],row[i])
             line2write+=(row[-1]+"\n")
             out.write(line2write)
