d7f2be71c5463766ac3e5b67ff622dab7651fb5c
Jeff Cole <coleti16@students.ecu.edu>
2018-01-21 13:43:19 -0500

    back to normal

--- normalize.py
+++ normalize.py
@@ -41,12 +41,12 @@ with open(sys.argv[1].replace("csv","norm.csv"),"w") as out:
                 # 2* (x-min)/(max)
                 line2write+=str( 2.0*(float(row[i]) - min_dict[i]) / (max_dict[i]-min_dict[i]) -1.0)+","
                 # print(str( 2.0*(float(row[i]) - min_dict[i]) / (max_dict[i]-min_dict[i])),min_dict[i],max_dict[i],row[i])
-            if row[-1] == "0":
-                line2write += "1\n"
-            if row[-1] == "1":
-                line2write += "0\n"
+            # if row[-1] == "0":
+            #     line2write += "1\n"
+            # if row[-1] == "1":
+            #     line2write += "0\n"
 
 
 
-            # line2write+=(row[-1]+"\n")
+            line2write+=(row[-1]+"\n")
             out.write(line2write)
